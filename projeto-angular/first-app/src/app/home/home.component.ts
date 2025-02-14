1709bc82c17bdd5f96b333225f3d80b3cad5b9c4
gabriel-rocha-pimentel <gabrielrochapimentel.dev@gmail.com>
2025-02-14 18:37:45 -0300

    create new component

--- projeto-angular/first-app/src/app/home/home.component.ts
+++ projeto-angular/first-app/src/app/home/home.component.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
+import { HousingLocationComponent } from '../housing-location/housing-location.component';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [
+    HousingLocationComponent,
+  ],
   template: `
     <section>
       <form>
         <input type="text" placeholder="Filter by city" required>
         <button class="primary" type="submit">Search</button>
       </form>
+    </section>
+    <section class="results">
+      <app-housing-location></app-housing-location>
     </section>
   `,
   styleUrls: ['./home.component.css'],
---
+++ projeto-angular/first-app/src/app/housing-location/housing-location.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-housing-location',
+  imports: [],
+  template: `
+    <h1>housing-location works!</h1>
+  `,
+  styleUrls: ['./housing-location.component.css'],
+})
+export class HousingLocationComponent { }
